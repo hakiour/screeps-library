c2dccebcbcd0173482ca8ef67ae3f28842dd0df4
Hamza Akiour <akiourh@gmail.com>
2018-01-20 20:51:48 +0100

    Delete takeresource.js

--- basics/takeresource.js
+++
@@ -1,8 +0,0 @@
-//Take the resource from the nearest source
-module.exports.loop = function () {
-    var creep = Game.creeps['Harvester1'];
-    var sources = creep.room.find(FIND_SOURCES);
-    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[0]);
-    }
-}
