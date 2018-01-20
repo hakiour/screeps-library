9d85b9752a65da8c8858eedf342eeeb0dc396770
Hamza Akiour <akiourh@gmail.com>
2018-01-20 16:23:31 +0100

    Create takeresource.js

---
+++ takeresource.js
@@ -0,0 +1,8 @@
+//Take the resource from an energy source
+module.exports.loop = function () {
+    var creep = Game.creeps['Harvester1'];
+    var sources = creep.room.find(FIND_SOURCES);
+    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0]);
+    }
+}
