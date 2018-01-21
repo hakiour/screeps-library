f16bb765eb9919cb73dc89c4a2d5cec9a6ab21c4
Hamza Akiour <akiourh@gmail.com>
2018-01-22 00:25:40 +0100

    Update role.harvester.js

--- basics/role.harvester.js
+++ basics/role.harvester.js
@@ -1,41 +1,35 @@
 var roleHarvester = {
-
+   
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.carry.energy < creep.carryCapacity) {
-           
-            if(creep.memory.mining == false)//If we are not mining
-            {
-                for(let sourcesToHarvest of creep.room.find(FIND_SOURCES)) {//Fin an avaiable resource 
-                    var sourceToHarvest_ID = sourcesToHarvest.id;
-                    if(Memory.sourceToHarvest_ID == false && creep.harvest(sourcesToHarvest) == ERR_NOT_IN_RANGE) {
-                        creep.memory.mining = sourceToHarvest_ID; 
-                        Memory.sourceToHarvest_ID = true;
-                        creep.moveTo(sourcesToHarvest, {visualizePathStyle: {stroke: '#ffaa00'}});
-                    }
-            }
-            }{//we still mining.
-                 var sources = creep.room.find(FIND_SOURCES);
-                  if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-                }
-            }
+
+        if(creep.memory.upgrading && creep.carry.energy == 0) {
+            creep.memory.upgrading = false;
+            creep.say('🔄 harvest');
         }
-        else {//we are full on carry capacity
-            var sourceHarvested = Game.getObjectById(creep.memory.mining);
-            Memory.sourceHarvested = false;
-            creep.memory.mining = false;
+        if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.upgrading = true;
+            creep.say('⚡ UP-Boss');
+        }
+
+        if(creep.memory.upgrading) {
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_EXTENSION ||
                         structure.structureType == STRUCTURE_SPAWN ||
-                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity || structure.structureType == STRUCTURE_SPAWN;
+                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
                 }
             });
             if(targets.length > 0) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
+            }
+        }
+        else {
+            var nearSource = creep.pos.findClosestByRange(creep.room.find(FIND_SOURCES_ACTIVE));
+            if(creep.harvest(nearSource) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(nearSource, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
     }
