dad7840e7ee9db5caf9edd6e776133044f16529d
Hamza Akiour <akiourh@gmail.com>
2018-01-22 20:44:50 +0100

    Add files via upload

--- basics/role.upgrader.js
+++ basics/role.upgrader.js
@@ -14,11 +14,12 @@ var roleUpgrader = {
 
         if(creep.memory.upgrading) {
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.say('⚡ MV-To-Controller');
                 creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
             }
         }
         else {
-            var nearSource = creep.pos.findClosestByRange(creep.room.find(FIND_SOURCES_ACTIVE));
+            var nearSource = creep.pos.findClosestByPath(creep.room.find(FIND_SOURCES_ACTIVE));
             if(creep.harvest(nearSource) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(nearSource, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
