2dfc6a5ed284f3a2c1e4a74d59de75cad4d410b3
Hamza Akiour <akiourh@gmail.com>
2018-02-04 02:43:36 +0100

    Add files via upload

--- rols/role.farmer.js
+++ rols/role.farmer.js
@@ -1,41 +1,37 @@
 var genericFunctions = require('genericFunctions');
-var roleHarvester = {
+var roleFarmer = {
    
     /** @param {Creep} creep **/
-    run: function(creep) {
-            genericFunctions.harvestNearSource(creep);
-    }
-
-    /*
-    var roleUpgrader = require('role.upgrader');
-	var roleRecolector = {
-    run: function(creep) {
-        var targets = creep.room.find(FIND_STRUCTURES,{
-            filter: (structure) => {return (structure.structureType == STRUCTURE_EXTENSION  || structure.structureType == STRUCTURE_SPAWN) && structure.energy != structure.energyCapacity }
-        }); 
-   
-        if(creep.carry.energy == creep.carryCapacity) {
-                       var resultado = creep.pickup(sources);
-
-            if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                 creep.moveTo(targets[0]);
-            } else if(resultado != OK){
-                roleUpgrader.run(creep);
-
-            }
+    run: function(creep) {      
+        var assignetToCreep = false;
+        var idSource = "";
+        if(!creep.memory.sourceId || creep.memory.sourceId == undefined ){
+            //Find sources that aren't assigned to other farmers
+                for(var source of creep.room.find(FIND_SOURCES)){
+                    assignetToCreep = false;
+                    for(var thisCreep of creep.room.find(FIND_MY_CREEPS,{
+                        filter: (thisRole) => (thisRole.memory.role == 'farmer')
+                    })){
+                        if(creep.name != thisCreep.name){
+                            idSource = source.id
+                            if( thisCreep.memory.sourceId == idSource){
+                                assignetToCreep = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!assignetToCreep){
+                        creep.memory.sourceId = idSource;
+                        break;
+                    }
+                }               
+        //If we dosen't found some free source, get the minerals             
         }else{
-           var sources = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES,{
-            filter: (energy) => {return (energy.amount > 40  && energy.resourceType == RESOURCE_ENERGY)}
-            });
-            if(creep.pickup(sources) == ERR_NOT_IN_RANGE) {
+            var sources = Game.getObjectById(creep.memory.sourceId);
+            if(creep.harvest(sources) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
-        }    
+        }
     }
 };
-
-module.exports = roleRecolector;
-    */
-};
-
-module.exports = roleHarvester;
+module.exports = roleFarmer;
